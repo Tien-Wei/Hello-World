1d86715392e866a79e0c5847f6cccd1abd476aef
Tien-Wei <72882097+Tien-Wei@users.noreply.github.com>
2020-10-21 10:40:49 -0700

    Create game flow-chart.py

---
+++ Week 3 assignment.py/game flow-chart.py
@@ -0,0 +1,30 @@
+# Setting up the screen
+# start by using tutrle module to create graphics
+# cap Screen becuase that is class
+# need to set the screen's resolution
+# have to use tracer to stop window's from updating (speed up games)
+
+
+# need the ball to play the game 
+# use an turtle object
+# use speed to adjust speed of animation
+# set the starting posistion of the ball at (0.0)
+# decide the speed of the ball moving
+
+
+# need paddles to play the game as well
+# same set up as the ball but have to change it's shape so uses the shape size command
+# need to set the posistion according to the resolution 800*600
+
+# set a loop for the game which uses
+# while True:win.update()
+# the ball have to move each frame. base on the old cor
+# check out the borders of the game, and when the ball hit the top and bottom borders, it will bounce back.
+# but when it touch the left and right corner it will reset to (0,0)
+
+# paddle and ball collisions
+# set up the condition of the collisions and make the ball to bounce to another direction
+
+# Set the keyboard binding
+# use the listen command to let the system listen for keyboard 
+# set the key for paddle 1 and 2's (up,down)
